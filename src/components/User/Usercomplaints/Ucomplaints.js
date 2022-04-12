087623851f0b1de55fe95400709b0ab4831b6d9d
ckvaizz <vaisakh.k591@gmail.com>
2022-04-12 20:28:00 +0530

    connected complaint with server

--- src/components/User/Usercomplaints/Ucomplaints.js
+++ src/components/User/Usercomplaints/Ucomplaints.js
@@ -10,28 +10,71 @@ import Box from "@mui/material/Box";
 const Ucomplaints = () => {
   const [complaints, setComplaints] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [options, setOptions] = useState("All");
   useEffect(() => {
     setLoading(true);
-    Axios.get("/complaint/get/All")
-      .then(({ data }) => {
-        setLoading(false);
-        if (data.status) {
-          setComplaints(data.complaint);
-        } else infoToast(data.message || "something wrong");
-      })
-      .catch((e) => {
-        setLoading(false);
-        errorToast("something wrong");
-      });
-  }, []);
+    if (options === "All") {
+      Axios.get("/complaint/get/All")
+        .then(({ data }) => {
+          setLoading(false);
+          if (data.status) {
+            setComplaints(data.complaint);
+          } else infoToast(data.message || "something wrong");
+        })
+        .catch((e) => {
+          setLoading(false);
+          errorToast("something wrong");
+        });
+    } else {
+      Axios.get("/complaint/get/Own")
+        .then(({ data }) => {
+          setLoading(false);
+          if (data.status) {
+            if (options === "Solved") {
+              setComplaints(
+                data.complaint.filter((i) => i.status === "solved")
+              );
+            } else if (options === "Blocked")
+              setComplaints(
+                data.complaint.filter((i) => i.status === "blocked")
+              );
+            else setComplaints(data.complaint);
+          } else infoToast(data.message || "something wrong");
+        })
+        .catch((e) => {
+          setLoading(false);
+          errorToast("something wrong");
+        });
+    }
+  }, [options]);
 
   return (
     <div className="ucom-box-main">
       <div className="ucom-btns">
-        <button>All</button>
-        <button>My Complaints</button>
-        <button>Solved</button>
-        <button>Blocked</button>
+        <button
+          onClick={() => setOptions("All")}
+          style={{ background: options === "All" && "#ab72dc" }}
+        >
+          All
+        </button>
+        <button
+          onClick={() => setOptions("Mycom")}
+          style={{ background: options === "Mycom" && "#ab72dc" }}
+        >
+          My Complaints
+        </button>
+        <button
+          onClick={() => setOptions("Solved")}
+          style={{ background: options === "Solved" && "#ab72dc" }}
+        >
+          Solved
+        </button>
+        <button
+          onClick={() => setOptions("Blocked")}
+          style={{ background: options === "Blocked" && "#ab72dc" }}
+        >
+          Blocked
+        </button>
       </div>
       <div className="ucom-content-main">
         {loading && (
@@ -49,19 +92,26 @@ const Ucomplaints = () => {
           </div>
         )}
         {complaints.length !== 0 &&
+          !loading &&
           complaints.map((item) => {
             return (
               <div className="com-box">
                 <p>{item.message}</p>
+                {
+                  options ==='All' &&
                 <div className="report-btn">
                   <ReportIcon />
                 </div>
+                }
+                {
+                  options !== 'All' && item.reply &&
                 <div className="reply-area">
                   <h6>Reply</h6>
                   <p>
-                    Vaiskah is gonna get suspension and de-barred immediatly
+                    {item.reply}
                   </p>
                 </div>
+                }
               </div>
             );
           })}
