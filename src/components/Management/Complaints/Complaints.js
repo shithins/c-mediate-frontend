263f97e44eeb78340c31557fe89f3df3b099ab16
ckvaizz <vaisakh.k591@gmail.com>
2022-04-22 19:39:33 +0530

    connected management complaint

--- src/components/Management/Complaints/Complaints.js
+++ src/components/Management/Complaints/Complaints.js
@@ -1,18 +1,58 @@
-import React, {  useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Complaints.css";
-import samimg from "../../../images/img1.webp"
+import samimg from "../../../images/img1.webp";
+import Axios from "../../../constant/axios";
+import { errorToast, infoToast } from "../../../constant/toast";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+
 const Mcomplaints = () => {
-    const [options, setOptions] = useState("All");
-    return(
-       <div className="mcom-box-main">
-       <div className="mcom-btns">
+  const [options, setOptions] = useState("All");
+  const [loading, setLoading] = useState(false);
+  const [complaints, setComplaints] = useState([]);
+
+  useEffect(() => {
+    if (options === "All") {
+      setComplaints([]);
+      setLoading(true);
+      Axios.get("/complaint/get/All")
+        .then(({ data }) => {
+          setLoading(false);
+          if (data.status) {
+            setComplaints(data.complaint);
+          } else infoToast(data.message || "something wrong please try again");
+        })
+        .catch((e) => {
+          setLoading(false);
+          errorToast("Please check your network");
+        });
+    }
+    if (options === "Blocked") {
+      setComplaints([]);
+      setLoading(true);
+      Axios.get("/complaint/getAllBlocked")
+        .then(({ data }) => {
+          setLoading(false);
+          if (data.status) {
+            setComplaints(data.complaint);
+          } else infoToast(data.message || "something wrong please try again");
+        })
+        .catch((e) => {
+          setLoading(false);
+          errorToast("Please check your network");
+        });
+    }
+  }, [options]);
+  return (
+    <div className="mcom-box-main">
+      <div className="mcom-btns">
         <button
           onClick={() => setOptions("All")}
           style={{ background: options === "All" && "#ab72dc" }}
         >
           All
         </button>
-    
+
         <button
           onClick={() => setOptions("Blocked")}
           style={{ background: options === "Blocked" && "#ab72dc" }}
@@ -22,27 +62,39 @@ const Mcomplaints = () => {
       </div>
 
       <div className="mcom-content-main">
-        
-
+        {loading && (
           <div className="mcom-box">
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-          when an unknown printer took a galley of type and scrambled it to make a 
-          
-            </p>
-           <img src={samimg} />
-        <div className="status-btns"> 
-        <button>Reply</button>
-        <button>Solved</button>
-        <button>Block request</button>
-
-        </div>
+            <center>
+              <Box>
+                <CircularProgress />
+              </Box>
+            </center>
           </div>
+        )}
+        {!loading && complaints.length === 0 && (
+          <div className="mcom-box">
+            <center>No complaints</center>
+          </div>
+        )}
 
+        {complaints.map((com) => {
+          return (
+            <div className="mcom-box" key={com._id}>
+              <p>{com.message}</p>
+              {com.image && (
+                <img src={com.image.url} alt="image not supported" />
+              )}
+              <div className="status-btns">
+                <button>Reply</button>
+                <button>Solved</button>
+                <button>Block request</button>
+              </div>
+            </div>
+          );
+        })}
       </div>
-       
-       </div>
-    );
+    </div>
+  );
 };
 
 export default Mcomplaints;
